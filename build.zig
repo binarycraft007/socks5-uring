e2cc3ef282eefa0471af769b8c1b663fe0985c41
Binary Craft <elliot.huang.signed@gmail.com>
2023-02-23 19:17:27 +0800

    initial commit

---
+++ build.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "socks5-io_uring",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addCSourceFiles(&[_][]const u8{"src/socks.c"}, &[_][]const u8{"-Wall"});
+    exe.linkSystemLibrary("liburing");
+    exe.linkLibC();
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    exe.install();
+}
